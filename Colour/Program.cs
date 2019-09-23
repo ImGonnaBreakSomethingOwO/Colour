93a705c3c14c8f75335df38819aacc7da49c1f5a
Student17 <student17@COMPUTING-GLOS.local>
2019-09-23 15:48:40 +0100

    Add project files.

---
+++ Colour/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Colour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Coloured Text Example";//RUN THE PROGRAM AND SEE WHAT EFFECT THIS HAS ON THE PROGRAM WINDOW 
+            string sName = "";
+
+            Console.WriteLine("Enter your name:");
+            sName = Console.ReadLine();
+
+            //THE BACKGROUNDCOLOR AND FOREGROUND COLOR ARE SELF DOCUMENTING!
+            //RUN THE PROGRAM AND CHECK THEM OUT
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            
+
+
+
+
+            Console.WriteLine("You have entered: \"{0}\"", sName);//TRY THIS WITHOUT THE ESCAPE CHARACTERS
+            Console.ResetColor();//SEE WHAT HAPPENS IF YOU DO NOT HAVE THIS LINE
+            Console.WriteLine(" ----------------------------- ");
+            //Console.BackgroundColor = ConsoleColor.Red;
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Please press the enter key to continue");
+            Console.ReadLine();
+            
+            //COMMENT OUT READLINE() AND REMOVE THE COMMENT CHARACTERS FOR READKEY()
+            //SEE WHAT EFFECT THAT HAS AND CHANGE THE PROGRAM AS NECESSARY
+            //Console.ReadKey();
+        }
+
+    }
+}
+
